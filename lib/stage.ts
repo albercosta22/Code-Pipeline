0118c2a6f1fa85d1ad6b208fe5b2a2dee4cf962e
albercosta22 <133118310+albercosta22@users.noreply.github.com>
2023-05-29 13:16:47 +0200

    Add files via upload

---
+++ lib/stage.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { MyLamdaStack } from './lambda-stack';
+
+//Definición de la clase stage/etapa
+export class MyPipelineAppStage extends cdk.Stage {
+    constructor(scope: Construct, stageName: string, props?: cdk.StackProps) {
+        super(scope, stageName, props);
+
+        const lambdaStack = new MyLamdaStack(this, 'LambdaStack', stageName);
+    }
+}
